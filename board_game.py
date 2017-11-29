b1eec9ddf6539ff1249174be49a878b7362b0b88
Grace <gkchan09@gmail.com>
2017-11-29 06:02:23 +0000

    Add print board function

--- board_game.py
+++ board_game.py
@@ -3,11 +3,19 @@ class Board():
 
     def __init__(self):
         """Creates board"""
-        
+
         self.board = []
         for x in range(3):
             self.board.append(3*[[]])
         for row in self.board:
             print row
 
+    def print_board(self):
+        """Prints board"""
+
+        for row in self.board:
+            print row
+
+
 board = Board()
+board.print_board()
