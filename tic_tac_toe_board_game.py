60960e7f68241ade4d3b12d6309e8dc5bb1905b6
Grace <gkchan09@gmail.com>
2017-12-11 03:29:15 +0000

    Add error handling for inputs not in range

--- tic_tac_toe_board_game.py
+++ tic_tac_toe_board_game.py
@@ -26,49 +26,71 @@ class Board():
 
         return self.board[row][column] == ""
 
+    # def is_valid(self, index):
+    #     """Check if index is valid"""
+
+        # if index not in range(3):
+
+
+
 
 board = Board()
 board.print_board()
+row = None
+column = None
 
+# do while?
 
 while True: 
     row = int(raw_input("Player 1, please choose a row."))
+    while row not in range(3):
+        # print "Input not valid"
+        row = int(raw_input("Player 1, please choose a row."))
+
     column = int(raw_input("Player 1, please choose a column."))
+    while column not in range(3):
+        column = int(raw_input("Player 1, please choose a column."))
     if board.is_empty(row, column):
         board.put_symbol("O", row, column)
         board.print_board()
     else:
         pass
-    # row = int(raw_input("Player 2, please choose a row."))
-    # column = int(raw_input("Player 2, please choose a column."))
-    # if board.is_empty(row, column):
-    #     board.put_symbol("X", row, column)
-    #     board.print_board()
+
+    row = int(raw_input("Player 2, please choose a row."))
+    while row not in range(3):
+        row = int(raw_input("Player 2, please choose a row."))
+    
+    column = int(raw_input("Player 2, please choose a column."))
+    while column not in range(3):
+        column = int(raw_input("Player 2, please choose a column."))
+    if board.is_empty(row, column):
+        board.put_symbol("X", row, column)
+        board.print_board()
         
 
     # horizontal win:
-    # # if "" not in board.board[0]
-    # print set(board.board[0])
-    # for i in range(0, 3):
-    #     if set(board.board[i]) == set(["O"]) or set(board.board[i]) == set(["X"]):
-    #         print "win"
+    # if "" not in board.board[0]
+    print set(board.board[0])
+    for i in range(0, 3):
+        if set(board.board[i]) == set(["O"]) or set(board.board[i]) == set(["X"]):
+            print "win"
 
 
-    # vert_set = set()
-    # for i in range(0, 3):
-    #     for j in range(0, 3):
-    #         vert_set.add(board.board[j][i])
-    #     print vert_set
-    #     if vert_set == set(["O"]) or vert_set == set(["X"]):
-    #         print "vertical win"
-    #     vert_set = set()
+    vert_set = set()
+    for i in range(0, 3):
+        for j in range(0, 3):
+            vert_set.add(board.board[j][i])
+        print vert_set
+        if vert_set == set(["O"]) or vert_set == set(["X"]):
+            print "vertical win"
+        vert_set = set()
 
-    # diagonal_set = set()
-    # diagonal_set.add(board.board[0][0])
-    # diagonal_set.add(board.board[1][1])
-    # diagonal_set.add(board.board[2][2])
-    # if diagonal_set == set(["O"]) or diagonal_set == set(["X"]):
-    #     print "diagonal win"
+    diagonal_set = set()
+    diagonal_set.add(board.board[0][0])
+    diagonal_set.add(board.board[1][1])
+    diagonal_set.add(board.board[2][2])
+    if diagonal_set == set(["O"]) or diagonal_set == set(["X"]):
+        print "diagonal win"
 
 
     diagonal_set = set()
